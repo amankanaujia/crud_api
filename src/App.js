431f4d41dd72ccc98c607a17c8a0a99a5d16a199
amankanaujia <amankanaujia777@gmail.com>
2024-04-05 10:18:38 +0530

    json-server dependency added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [readData, setReadData] = useState();
+  const [id, setId] = useState();
+
+  const read = async () => {
+    const response = await fetch("http://localhost:8080/Users");
+    const data = await response.json();
+    setReadData(data);
+    console.log(data);
+  };
+
+  const create = async () => {
+    const response = await fetch("http://localhost:8080/Users", {
+      method: "POST",
+      body: JSON.stringify({
+        id: 6,
+        name: "aman",
+        country: "india",
+        comment: "good",
+        tandc: false,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    // setReadData(data);
+    console.log(data);
+  };
+
+  const update = async () => {
+    const response = await fetch("http://localhost:8080/Users/6", {
+      method: "PUT",
+      body: JSON.stringify({
+        name: "aman kanaujia 007",
+        country: "india",
+        comment: "good",
+        tandc: false,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    // setReadData(data);
+    console.log(response);
+  };
+
+  const deleteFunction = async () => {
+    const response = await fetch("http://localhost:8080/Users/6", {
+      method: "DELETE",
+    });
+    // setReadData(data);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    read();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Read Data</h1>
+        <div>
+          {readData?.map((data) => {
+            return (
+              <>
+                <p>
+                  {data.id}
+                  {data.name}
+                </p>
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <button onClick={create}>create</button>
+      <button onClick={update}>update</button>
+      <button onClick={deleteFunction}>delete</button>
+    </>
   );
 }
 
